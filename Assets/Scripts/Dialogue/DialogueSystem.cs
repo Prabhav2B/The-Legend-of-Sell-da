3bbf9dc69824859e450ec075860852a60d7a4509
Prabhav Bhatnagar <prabhav2b@gmail.com>
2022-08-31 15:25:05 +0300

    dialogue proof of concept

--- Assets/Scripts/Dialogue/DialogueSystem.cs
+++ Assets/Scripts/Dialogue/DialogueSystem.cs
@@ -1,18 +1,73 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.InputSystem;
 
 public class DialogueSystem : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]private TextMeshProUGUI textComponent;
+    [SerializeField] private string[] lines;
+    
+    [Range(.1f, 100f)]
+    [SerializeField] private float textSpeed;
+
+    private int index;
+
+    public float TextSpeed => 1f / textSpeed;
+    
+    private void Start()
     {
+        textComponent.text = String.Empty;
+        StartDialogue();
+    }
+
+
+    public void OnDialogueAction(InputAction.CallbackContext context)
+    {
+        if(!context.performed)
+            return;
+        
+        if (textComponent.text == lines[index])
+        {
+            NextLine();
+        }
+        else
+        {
+            StopAllCoroutines();
+            textComponent.text = lines[index];
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void StartDialogue()
     {
-        
+        StartCoroutine(TypeLine());
+        index = 0;
     }
+
+    IEnumerator TypeLine()
+    {
+        foreach (char c in lines[index].ToCharArray())
+        {
+            textComponent.text += c;
+            yield return new WaitForSeconds(TextSpeed);
+        }
+    }
+
+    void NextLine()
+    {
+        if (index < lines.Length - 1)
+        {
+            index++;
+            textComponent.text = String.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
 }
