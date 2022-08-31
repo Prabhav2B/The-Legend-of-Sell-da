6c05e77aa7c27ac7d44f8815c59dba471065a04c
Prabhav Bhatnagar <prabhav2b@gmail.com>
2022-08-31 14:50:55 +0300

    starting dialogue system

---
+++ Assets/Scripts/Miscellaneous/Enums.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Enums 
+{
+    public enum ItemTypes
+    {
+        bomb_arrows,
+        forest_dweller_shield,
+        forest_dweller_bow,
+        forest_dweller_sword,
+        fairy,
+        hylian_shield,
+        phantom_cape,
+        phantom_helmet,
+        sheik_mask,
+        ocarina,
+        majora_mask
+    }
+}
---
+++ Assets/Scripts/Scriptable Objects/ItemSO.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/Items", order = 1)]
+public class ItemSO : ScriptableObject
+{
+    public string itemName;
+    public string description;
+    public Enums.ItemTypes itemType;
+}
