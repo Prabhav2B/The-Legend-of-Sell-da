670ace4230d552a23ac41a6c6cc023145e8416a7
A57R4L <ossi.luoto@gmail.com>
2022-09-01 19:34:56 +0300

    ItemInfo update + some inventory rework to spawn right prefabs

--- Assets/Scripts/Dialogue/DialogueSystem.cs
+++ Assets/Scripts/Dialogue/DialogueSystem.cs
@@ -20,7 +20,7 @@ public class DialogueSystem : MonoBehaviour
 
     private void Awake()
     {
-        gameObject.SetActive(false);
+    //    gameObject.SetActive(false);
         textComponent.text = String.Empty;
         textComponentDescription.text = String.Empty;
     }
@@ -65,12 +65,12 @@ public class DialogueSystem : MonoBehaviour
 
     public void SetItemDescription(ItemSO item)
     {
-        textComponent.text = item.description;
+        textComponentDescription.text = item.description;
     }
     
     public void ClearItemDescription()
     {
-        textComponent.text = String.Empty;
+        textComponentDescription.text = String.Empty;
     }
 
     IEnumerator TypeLine()
